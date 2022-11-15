62ba5c12535e706030ed930b94ed1c52b4164211
Александр Фокин <wegnagun@bk.ru>
2022-11-15 17:48:30 +0300

    change boolean filter for char filter in filters -_-

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -15,10 +15,10 @@ class IngredientSearchFilter(filters.FilterSet):
 class RecipeFilter(filters.FilterSet):
     """ Фильтр для рецептов и тегов. """
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(
+    is_favorited = filters.CharFilter(
         field_name='is_favorited', method='get_is_favorited'
     )
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_shopping_cart = filters.CharFilter(
         field_name='is_in_shopping_cart', method='get_is_in_shopping_cart'
     )
     author = filters.AllValuesMultipleFilter(field_name='author__id')
@@ -28,15 +28,11 @@ class RecipeFilter(filters.FilterSet):
         fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
 
     def get_is_favorited(self, queryset, name, value):
-        print(name)
-        if self.request.query_params.get(name):
-            print('udacha')
-        # if 'is_favorited' in value:
-        # if value in [True, 1, '1']:
+        if value in [True, '1']:
             return queryset.filter(favoriting__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
+        if value in [True, '1']:
             return queryset.filter(purchases__user=self.request.user)
         return queryset
